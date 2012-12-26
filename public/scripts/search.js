84c7cc0b6995c63f2c5d2a101197af69a14f3c6f
Jonathan Haber <HiJon89@gmail.com>
2012-12-26 10:31:52 -0500

    Show some covers when searching

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
-  $('#search').fadeIn(1000, function() {
-    $('#search').focus();
-  });
+    var searchBox = $('#search');
+
+    $('.form-search').submit(function(event) {
+        event.preventDefault();
+    });
+
+    searchBox.fadeIn(1000, function() {
+        searchBox.focus();
+    });
+
+    searchBox.keyup(function(event) {
+        if (event.keyCode == 13) {
+            var query = searchBox.val();
+
+            var resultsContainer = $('#container-results');
+            resultsContainer.html('');
+            $.ajax({
+                url: 'http://filmsurf-backend.herokuapp.com/search',
+                type: 'GET',
+                data: { q: query },
+                success: function(results) {
+                    appendResults(results.results, resultsContainer);
+                }
+            });
+        }
+    });
 });
+
+function appendResults(results, container) {
+    $.each(results, function(index, result) {
+        if (result.poster_path) {
+            var div = '<div class="span2"><img src="http://cf2.imgobject.com/t/p/w154' +
+                result.poster_path + '" id="' + result.id + '" alt="' + result.title + '"/></div>';
+            container.append(div);
+        }
+    });
+}
